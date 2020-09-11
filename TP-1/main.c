26b7f02eff3049cdf3648c3828d02b41fed4a38d
Micaela Acuña <micahl1981@gmail.com>
2020-09-11 00:54:54 -0300

    tp1

--- TP-1/main.c
+++ TP-1/main.c
@@ -4,55 +4,134 @@
 #include <conio.h>
 
 /*
-1. Ingresar 1er operando (A=x)
-2. Ingresar 2do operando (B=y)
-3. Calcular todas las operaciones
-a) Calcular la suma (A+B)
-b) Calcular la resta (A-B)
-c) Calcular la division (A/B)
-d) Calcular la multiplicacion (A*B)
-e) Calcular el factorial (A!)
-4. Informar resultados
-a) “El resultado de A+B es: r”
-b) “El resultado de A-B es: r”
-c) “El resultado de A/B es: r” o “No es posible dividir por cero”
-d) “El resultado de A*B es: r”
-e) “El factorial de A es: r1 y El factorial de B es: r2”
-5. Salir
+1. Ingresar 1er operando (A=x)\n2. Ingresar 2do operando (B=y\n3. Calcular todas las operaciones\na) Calcular la suma (A+B)\nb) Calcular la resta (A-B)\nc) Calcular la division (A/B)\nd) Calcular la multiplicacion (A*B)\ne) Calcular el factorial (A!)\n4. Informar resultados\na) “El resultado de A+B es: r”\nb) “El resultado de A-B es: r”\nc) “El resultado de A/B es: r”\nd) “El resultado de A*B es: r”\ne) “El factorial de A es: r1 y El factorial de B es: r2”\n 5. Salir\n
 */
+
+
+/**
+* / brief Solicita un numero al usuario, valida, verifica y devuelve el resultado
+* / param pResultado se guarda el numero ingresado por el usuario
+* / param mensaje es el mensaje que se mostrara
+* / param mensajeError mensaje de error si hay un error
+* / param minimo valor aceptado
+* / param maximo valor aceptado
+* / return Exitoso = 0. Error = -1
+*/
+int getOption(int* pResultado, char* mensaje, char* mensajeError, int minimo, int maximo);
 
 int obtNum();
 int suma(int a, int b);
 int resta(int a, int b);
-int multiplicacion(int a, int b);
+long long int multiplicacion(int a, int b);
 long long int factorial(int a);
-float division(int a, int b);
+int division(float* pResultado,int a, int b);
 
 
 int main()
 {
-    char seguir = 's';
     int opcion;
     int x;
     int y;
     int opc1 = 0;
     int opc2 = 0;
     int opc3 = 0;
+    int respuesta;
 
-    printf("******Menu de opciones******\n");
-    printf("1. Ingresar 1er operando (A=x)\n");
-    printf("2. Ingresar 2do operando (B=y)\n");
-    printf("3. Calcular todas las operaciones: \n");
-    printf("   a) Calcular la suma (A+B)\n");
-    printf("   b) Calcular la resta (A-B)\n");
-    printf("   c) Calcular la division (A/B)\n");
-    printf("   d) Calcular la multiplicacion (A*B)\n");
-    printf("   e) Calcular el factorial (A!)\n");
-    printf("4. Informar resultados\n");
-    printf("5. Salir\n\n");
+    respuesta = getOption(&opcion, "1. Ingresar 1er operando (A=x)\n2. Ingresar 2do operando (B=y)\n3. Calcular todas las operaciones\na) Calcular la suma (A+B)\nb) Calcular la resta (A-B)\nc) Calcular la division (A/B)\nd) Calcular la multiplicacion (A*B)\ne) Calcular el factorial (A!)\n 4. Informar resultados\n a) El resultado de A+B es: r\n b) El resultado de A-B es: r\n c) El resultado de A/B es: r\n d) El resultado de A*B es: r\n e) El factorial de A es: r1 y El factorial de B es: r2\n 5. Salir.\n", "Opción invalida", 1, 5);
 
+    if(!respuesta)
+    {
+        do
+        {
+            printf("Ingrese opcion\n\n");
 
-    do
+            switch(opcion)
+            {
+            case 1:
+                //Obtener numero
+                x = obtNum();
+                opc1 = 1;
+                break;
+            case 2:
+                //Obtener otro numero
+                y = obtNum();
+                opc2 = 1;
+                break;
+            case 3:
+                if(!opc1 || !opc2)
+                {
+                    printf("Primero debes ingresar ambos operandos");
+                }
+                else
+                {
+                    opc3 = 1;
+                    printf("Calculando...");
+                    system("pause");
+                }
+                break;
+            case 4:
+            {
+                if(!opc3)
+                {
+                    printf("Aun no hiciste los calculos...");
+                    break;
+                }
+
+                float resultadoDivision;
+                int divisionError = division(&resultadoDivision, x, y);
+                if(!divisionError)
+                {
+                    system("cls");
+                    printf("******Menu de opciones******\n");
+                    printf("1. Ingresar 1er operando (A = %d)\n", x);
+                    printf("2. Ingresar 2do operando (B = %d)\n", y);
+                    printf("3. Calcular todas las operaciones\n");
+                    printf("   a) Calcular la suma (A+B)\n");
+                    printf("   b) Calcular la resta (A-B)\n");
+                    printf("   c) Calcular la division (A/B)\n");
+                    printf("   d) Calcular la multiplicacion (A*B)\n");
+                    printf("   e) Calcular el factorial (A!)\n");
+                    printf("4. Informar resultados\n");
+                    printf("   a) Resultado de (A+B): %d\n", suma(x, y));
+                    printf("   b) Resultado de (A-B): %d\n", resta(x, y));
+                    printf("   c) Division de (A/B): %f\n", resultadoDivision);
+                    printf("   d) Multiplicacion de (A*B): %lld\n", multiplicacion(x, y));
+                    printf("   e) Factorial (A!): %lld\n", factorial(x));
+                    printf("5. Salir\n\n");
+                }
+                else
+                {
+                    system("cls");
+                    printf("******Menu de opciones******\n");
+                    printf("1. Ingresar 1er operando (A = %d)\n", x);
+                    printf("2. Ingresar 2do operando (B = %d)\n", y);
+                    printf("3. Calcular todas las operaciones\n");
+                    printf("   a) Calcular la suma (A+B)\n");
+                    printf("   b) Calcular la resta (A-B)\n");
+                    printf("   c) Calcular la division (A/B)\n");
+                    printf("   d) Calcular la multiplicacion (A*B)\n");
+                    printf("   e) Calcular el factorial (A!)\n");
+                    printf("4. Informar resultados\n");
+                    printf("   a) Resultado de (A+B): %d\n", suma(x, y));
+                    printf("   b) Resultado de (A-B): %d\n", resta(x, y));
+                    printf("   c) Division de (A/B): No se puede dividir por cero.\n");
+                    printf("   d) Multiplicacion de (A*B): %lld\n", multiplicacion(x, y));
+                    printf("   e) Factorial (A!): %lld\n", factorial(x));
+                    printf("5. Salir\n\n");
+                }
+                break;
+            }
+            case 5:
+                //Salir
+                printf("Saliendo...\n");
+                system("pause");
+                break;
+            }
+        }
+        while(opcion != 5);
+    }
+
+    /*do
     {
         printf("Ingrese opcion\n\n");
         scanf("%d", &opcion);
@@ -125,7 +204,7 @@ int main()
                 printf("4. Informar resultados\n");
                 printf("   a) Resultado de (A+B): %d\n", suma(x, y));
                 printf("   b) Resultado de (A-B): %d\n", resta(x, y));
-                printf("   c) Division de (A/B): %f\n", division(x, y));
+                //printf("   c) Division de (A/B): %f\n", division(x, y));
                 printf("   d) Multiplicacion de (A*B): %d\n", multiplicacion(x, y));
                 printf("   e) Factorial (A!): %lld\n", factorial(x));
                 printf("5. Salir\n\n");
@@ -142,11 +221,12 @@ int main()
         default:
             printf("\nOpcion invalida\n");
         }
-        system("pause");
+        //(system("pause");
     }
     while( seguir == 's');
 
     //printf("\nUsted ingreso opcion: %d\n", menu());
+    */
 
     return 0;
 }
@@ -173,9 +253,9 @@ int resta(int a, int b)
     return resultado;
 }
 
-int multiplicacion(int a, int b)
+long long int multiplicacion(int a, int b)
 {
-    int resultado = a * b;
+    long long int resultado = a * b;
     return resultado;
 }
 
@@ -189,19 +269,45 @@ long long int factorial(int a)
     return resultado;
 }
 
-float division(int a, int b)
+int division(float* pResultado,int a, int b)
 {
-    float resultado;
-    char error = 'n';
-    if(b == 0)
+    int error = -1;
+    if(b == 0 || b == 0)
     {
         return error;
     }
     else
     {
-        resultado = (float) a / b;
-        return resultado;
+        *pResultado = (float) a / b;
+        error = 0;
+        return error;
     }
+}
+
+
+int getOption(int* pResultado, char* mensaje, char* mensajeError, int minimo, int maximo)
+{
+    int retorno = -1;
+    int opcion;
+    if(pResultado != NULL && mensaje != NULL && mensajeError != NULL && minimo <= maximo)
+    {
+        do
+        {
+            printf("%s", mensaje);
+            scanf("%d", &opcion);
+            if(opcion >= minimo && opcion <= maximo)
+            {
+                *pResultado = opcion;
+                return 0;
+            }
+            else
+            {
+                printf("%s", mensajeError);
+            }
+        }
+        while(!retorno);
+    }
+    return retorno;
 }
 
 
